8da93257726703a14bd8fdecb2d136dc457b7917
amzesmoro <amzesmoro05@gmail.com>
2021-11-28 22:54:27 +0700

    [HTML to NextJS] Convert 404 Page

--- learn-web/storegg/pages/sign-up-success.tsx
+++ learn-web/storegg/pages/sign-up-success.tsx
@@ -108,7 +108,7 @@ export default function SignUpSuccess() {
         <div className="pt-70 pb-md-50 pb-150">
           <h2 className="text-4xl fw-bold text-center color-palette-1 mb-10">Well Done!</h2>
           <p className="text-lg text-center color-palette-1 m-0">Kamu sudah bisa melakukan top up<br
-            className="d-sm-block d-none"/>
+            className="d-sm-block d-none" />
             dan menjadi pemenang!</p>
         </div>
         <div className="button-group d-flex flex-column mx-auto">
